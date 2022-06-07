ece98454803af08aba1ff7399b4e5486acf0527a
rapolas.kutka <rapolas.kutka@nfq.lt>
2022-06-07 19:08:00 +0300

    Changed background and added description field

--- src/pages/Exhibit.js
+++ src/pages/Exhibit.js
@@ -23,6 +23,7 @@ const Exhibit = () => {
         setData({
           code: json.code,
           disc: json.disc,
+          name: json.name,
           imageName: json.image_name
         });
       })
@@ -42,7 +43,7 @@ const Exhibit = () => {
             justifyContent: 'center',
             alignItems: 'center'
           }}
-          color="#0071e9"
+          color="#2AD2AA"
         />
       ) : (
         <div>
@@ -55,9 +56,25 @@ const Exhibit = () => {
             disableKeyboardCtrl
             orientationOnByDefault={true}
           />
-          <div className="description">
-            <h1>Title of exhibit</h1>
-            <p>{data.disc}</p>
+          <div className="description-background">
+            <h1 className="margin-20">{data.name}</h1>
+            <div className="margin-10">{data.disc}</div>
+          </div>
+          <div style={{ position: 'absolute', bottom: '-5px' }}>
+            <svg
+              width={window.innerWidth}
+              viewBox="0 0 375 302"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M-74 343.863C-74 343.863 -74.0001 0 174 171.931C422 343.863 422 0 422 0L422 343.863L-74 343.863Z"
+                fill="#2AD2AA"
+              />
+              <path
+                d="M-74 381C-74 381 -74 37.1372 174 209.069C422 381 422 37.1372 422 37.1372L422 381L174 381L-74 381Z"
+                fill="#4EA095"
+              />
+            </svg>
           </div>
         </div>
       )}
